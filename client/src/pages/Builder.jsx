e9024cf767ba3221a678ac4f411d9ea2713c8ce0
RahulSingh104 <rahulsingh96287@gmail.com>
2026-03-18 21:16:10 +0530

    fix:mistakely adding code error

--- client/src/pages/Builder.jsx
+++ client/src/pages/Builder.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import API from "../services/api";
-import { set } from "mongoose";
+
 
 function Builder() {
 
